e9fe3d533081b904c091ccf737fe53234c9d0fea
mikiyas97 <mikiyashulualem@gmail.com>
2026-04-27 15:44:32 +0300

    Version 4.0: Implement local library hosting and offline Service Worker (v5)

--- version4.0/sw.js
+++ version4.0/sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'gpa-calc-v4';
+const CACHE_NAME = 'gpa-calc-v5';
+
 const ASSETS = [
   './',
   './index.html',
@@ -6,34 +7,35 @@ const ASSETS = [
   './script.js',
   './icon.svg',
   './manifest.json',
-  'https://cdn.jsdelivr.net/npm/chart.js'
+
+  // LOCAL libraries (IMPORTANT)
+  './js/chart.min.js',
+  './js/jspdf.min.js',
+  './js/html2canvas.js'
 ];
 
-// Install Service Worker
+// Install
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activate Service Worker
+// Activate
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
-    })
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+      )
+    )
   );
 });
 
-// Fetch Strategy: Cache First, then Network
+// Fetch (Cache First)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
